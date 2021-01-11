569e1eaac444a50f8527e074d335eb390cea116b
PeterHester <74400413+PeterHester@users.noreply.github.com>
2021-01-11 20:58:38 +0000

    Add files via upload

---
+++ NumsInArray.h
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+      //Q3 print the array of numbers and memory locations
+void printNumsinArray(int arrOfNums[],int NumsInArray){
+            cout<<endl<<endl;
+            cout<<"The Numbers in the array and there Memory location"<<endl;
+            for (int i=0;i<NumsInArray;i++){
+            cout<<arrOfNums[i]<<",-----------"<<&arrOfNums[i]<<endl;
+            }
+
+}
+
+
+
---
+++ Scalar.h
@@ -0,0 +1,15 @@
+//Q5 Create a function that will pass the array by reference and also the scalar value and multiply each number in the array by the scalar.
+   //void  arrayxScalar(int* scalarRef,int* arrOfNumsRef,int NumsInArray,int arrOfNums[],int scalar){
+
+
+  // for(int i=0;i<NumsInArray;i++){
+    //   arrOfNums[i]=(arrOfNums[i])*(scalar);
+
+
+
+  //  cout<<endl;
+
+
+//
+
+
